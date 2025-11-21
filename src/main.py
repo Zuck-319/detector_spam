da79e507136207f58f8cc30f0491ae4972e2158e
Espinosa Zamora Dylan Xochtiel <25460308@colima.tecnm.mx>
2025-11-21 11:36:25 -0600

    Commit 3

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+import  screen
+import spam_detector
 
 # 1. Cargar los datos
 import os
